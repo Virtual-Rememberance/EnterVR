eb8e2be2f283fce5f60ceeb763fe08fc6ca2e4c0
LukeIsCringe <lukejwatts05@gmail.com>
2025-04-01 13:07:13 +0100

    Camera Prefab + TMPro Package

---
+++ Assets/DetectCamera.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+
+public class DetectCamera : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI caughtText;
+    private bool caughtDisplayed;
+    private int caughtCount;
+    private bool caught;
+
+    private void Start()
+    {
+        caughtText.enabled = false;
+        caughtCount = 0;
+        caught = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Camera") && !caught)
+        {
+            Debug.Log("Caught");
+            StartCoroutine(Caught());
+            caught = true;
+        }
+    }
+
+    IEnumerator Caught()
+    {
+        while (caughtCount < 5)
+        {
+            caughtText.enabled = true;
+            yield return new WaitForSeconds(0.75f);
+            caughtText.enabled = false;
+            yield return new WaitForSeconds(0.75f);
+            caughtCount++;
+        }
+    }
+}
